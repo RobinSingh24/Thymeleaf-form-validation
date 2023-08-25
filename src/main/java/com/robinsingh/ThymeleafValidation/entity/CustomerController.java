401d6a449aaebb5ddbb94c933d741af471fc22e5
Robin SIngh <robin.122497@gmail.com>
2023-08-25 20:17:33 +0530

    Added customer confirmation logic

--- src/main/java/com/robinsingh/ThymeleafValidation/entity/CustomerController.java
+++ src/main/java/com/robinsingh/ThymeleafValidation/entity/CustomerController.java
@@ -1,15 +1,32 @@
 package com.robinsingh.ThymeleafValidation.entity;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
     @GetMapping("/")
     public String showForm(Model theModel){
         theModel.addAttribute("Customer", new Customer());
-        return "customer-from";
+        return "customer-form";
     }
+
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("Customer") Customer theCustomer,
+            BindingResult theBindingResult){
+
+        if(theBindingResult.hasErrors())
+            return "customer-form";
+        else
+            return "customer-confirmation";
+
+    }
+
 
 }
