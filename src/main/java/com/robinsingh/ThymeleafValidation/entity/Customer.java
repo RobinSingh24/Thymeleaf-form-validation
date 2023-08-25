5722e8f8e6d30c19a8883bb4af976e4712845a9d
Robin SIngh <robin.122497@gmail.com>
2023-08-26 00:40:28 +0530

    added Postal Code field

--- src/main/java/com/robinsingh/ThymeleafValidation/entity/Customer.java
+++ src/main/java/com/robinsingh/ThymeleafValidation/entity/Customer.java
@@ -1,7 +1,6 @@
 package com.robinsingh.ThymeleafValidation.entity;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +9,14 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @NotNull
+    @Min(value = 0,message = "Value must be greater than or equal to 0")
+    @Max(value = 10,message = "Value must be less than or equal to 0")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 digits/characters")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +32,19 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
